b43d65dcce7d002a433558fb04ed1c57821344af
Bri McOmber <bri.mcomber@gmail.com>
2020-09-11 14:52:59 -0700

    added complete urls.py file. added stand in fuctions for all in views.py

--- menu/urls.py
+++ menu/urls.py
@@ -4,5 +4,20 @@ from . import views
 
 app_name = 'menu'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('recipes', views.recipes, name='recipes'),
+    path('recipes/<int:recipe_id>', views.recipe, name='recipe'),
+    path('recipes/<int:recipe_id>/addgroceries', views.recipe_groceries, name='recipe_groceries'),
+    path('recipes/<int:recipe_id>/edit', views.edit_recipe, name='edit_recipe'),
+    path('recipes/new', views.new_recipe, name='newrecipe'),
+    path('groceries', views.groceries, name='groceries'),
+    path('mealplans', views.mealplans, name='mealplans'),
+    path('mealplans/new', views.new_meal_plan, name='new_meal_plan'),
+    path('mealplans/new/<int:recipe_id>', views.recipe_meal_plan, name='recipe_meal_plan'),
+    path('mealplans/<int:meal_plan_id>', views.focus_meal_plans, name='focus_meal_plans'),
+    path('mealplans/<int:meal_plan_id>/edit', views.edit_meal_plan, name='edit_meal_plan'),
+    path('mealplans/addgroceries', views.meal_plan_groceries, name='meal_plan_groceries'),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request): 
     return render(request, 'menu/index.html')
+
+def recipes(request):
+    return HttpResponse('<h1>recipes<h1>')
+
+def recipe(request):
+    return HttpResponse('<h1>recipe<h1>')
+
+def recipe_groceries(request):
+    return HttpResponse('<h1>recipe_groceries<h1>')
+
+def edit_recipe(request):
+    return HttpResponse('<h1>edit_recipe<h1>')
+
+def new_recipe(request):
+    return HttpResponse('<h1>new_recipe<h1>')
+
+def groceries(request):
+    return HttpResponse('<h1>groceries<h1>')
+
+def mealplans(request):
+    return HttpResponse('<h1>mealplans<h1>')
+
+def new_meal_plan(request):
+    return HttpResponse('<h1>new_meal_plan<h1>')
+
+def recipe_meal_plan(request):
+    return HttpResponse('<h1>recipe_meal_plan<h1>')
+
+def focus_meal_plans(request):
+    return HttpResponse('<h1>focus_meal_plans<h1>')
+
+def edit_meal_plan(request):
+    return HttpResponse('<h1>edit_meal_plan<h1>')
+
+def meal_plan_groceries(request):
+    return HttpResponse('<h1>meal_plan_groceries<h1>')
+
+def login_view(request):
+    return HttpResponse('<h1>login_view<h1>')
+
+def logout_view(request):
+    return HttpResponse('<h1>logout_view<h1>')
+
+def register(request):
+    return HttpResponse('<h1>register<h1>')
